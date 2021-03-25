82f6f449b91844af97695c665e6df29eb97659d2
Michał Nowotnik <michal@mnowotnik.com>
2021-03-25 22:06:26 +0100

    Add copyright in footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,9 +1,15 @@
-import { Box, Flex, Heading } from "@chakra-ui/react"
-import { Link } from "gatsby"
+import { Box } from "@chakra-ui/react"
 import React from "react"
 
 export default () => (
-  <Box as="footer" w="100%" p={8} bgColor="black">
-    footer
+  <Box
+    as="footer"
+    w="100%"
+    p={{ base: 4, md: 8 }}
+    fontSize={{ base: "xs", md: "sm" }}
+    bgColor="black"
+    color="whiteAlpha.700"
+  >
+    © 2021 Michał Nowotnik. All rights reserved.
   </Box>
 )
